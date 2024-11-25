01cf08428a2ea4cfec177d661df28476a0b6044e
박지민 <pjm5489@naver.com>
2024-11-25 18:11:59 +0900

    fix: 오타 수정

--- src/main/kotlin/christmas/domain/MenuBoard.kt
+++ src/main/kotlin/christmas/domain/MenuBoard.kt
@@ -1,11 +1,11 @@
 package christmas.domain
 
 enum class MenuBoard(val menuType: String,val menuName: String, val price: Int) {
-    MUSHROOMSOUP("에피타이저", "양송이수프", 6000),
-    TAPAS("에피타이저", "타파스", 5500),
-    CAESARSALAD("에피타이저", "시저샐러드", 8000),
+    MUSHROOMSOUP("애피타이저", "양송이수프", 6000),
+    TAPAS("애피타이저", "타파스", 5500),
+    CAESARSALAD("애피타이저", "시저샐러드", 8000),
     TONESTEAK("메인", "티본스테이크", 55000),
-    BARBECUERIBS("메인", "바베큐립", 54000),
+    BARBECUERIBS("메인", "바비큐립", 54000),
     SEAFOODPASTA("메인", "해산물파스타", 35000),
     CHRISTMASPASTA("메인", "크리스마스파스타", 25000),
     CHOCOLATECAKE("디저트", "초코케이크", 15000),
