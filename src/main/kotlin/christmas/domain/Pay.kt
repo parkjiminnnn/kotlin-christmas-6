a1c09e59ee4d555d5e0c45e779fc0eedd71d835f
박지민 <pjm5489@naver.com>
2024-11-25 18:12:21 +0900

    feat: 무료 증정 금액 기능 추가

--- src/main/kotlin/christmas/domain/Pay.kt
+++ src/main/kotlin/christmas/domain/Pay.kt
@@ -6,25 +6,31 @@ class Pay(private val calendar: Calendar, private val customer: Customer) {
 
     fun christmasDisCount(): Int {
         if (reserveDate in 1..25) {
-            return 1000 + (reserveDate - 1) * 100
+            return -(1000 + (reserveDate - 1) * 100)
         }
         return 0
     }
 
-    fun weekendDiscount(): Int {
-        val menuNames = matchedMenuBoard.map { it.menuName }
+     fun weekendDiscount(): Int {
+        val mainQuantitySum = customer.orders.filter { order ->
+            matchedMenuBoard.any { menuBoard ->
+                menuBoard.menuType == "메인" && menuBoard.menuName == order.menuName
+            }
+        }.sumOf { it.quantity }
         if (calendar.isWeekend()) {
-            val count = menuNames.count { it.contains("메인") }
-            return -(count * 2023)
+            return -(mainQuantitySum * 2023)
         }
         return 0
     }
 
     fun weekDayDiscount(): Int {
-        val menuNames = matchedMenuBoard.map { it.menuName }
-        val count = menuNames.count { it.contains("디저트") }
+        val dessertQuantitySum = customer.orders.filter { order ->
+            matchedMenuBoard.any { menuBoard ->
+                menuBoard.menuType == "디저트" && menuBoard.menuName == order.menuName
+            }
+        }.sumOf { it.quantity }
         if (!calendar.isWeekend()) {
-            return -(count * 2023)
+            return -(dessertQuantitySum * 2023)
         }
         return 0
     }
@@ -34,7 +40,7 @@ class Pay(private val calendar: Calendar, private val customer: Customer) {
         return 0
     }
 
-    private fun totalOrderPrice(): Int {
+    fun beforeDiscountPrice(): Int {
         val menuPrices = matchedMenuBoard
         var totalPrice = 0
         customer.orders.forEachIndexed { index, menu ->
@@ -44,6 +50,26 @@ class Pay(private val calendar: Calendar, private val customer: Customer) {
     }
 
     fun totalDisCount(): Int {
-        return weekendDiscount() + weekDayDiscount() + specialDiscount() + christmasDisCount()
+        return weekendDiscount() + weekDayDiscount() + specialDiscount() + christmasDisCount() + freeEventDiscount()
+    }
+
+    fun freeEventDiscount(): Int {
+        if (beforeDiscountPrice() >= 120000) return -25000
+        return 0
+    }
+
+    fun getBatch(): String {
+        val totalDiscount = -totalDisCount()
+        return when {
+            totalDiscount in 5000 until 10000 -> "별"
+            totalDiscount in 10000 until 20000 -> "트리"
+            totalDiscount >= 20000 -> "산타"
+            else -> "없음"
+        }
+    }
+
+    fun afterDiscountPrice(): Int {
+        return beforeDiscountPrice() + totalDisCount()
+
     }
 }
