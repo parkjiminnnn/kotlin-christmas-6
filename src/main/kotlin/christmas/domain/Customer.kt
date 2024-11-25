78f7f10e815a0b4316a5fc140094f5c893429c70
박지민 <pjm5489@naver.com>
2024-11-25 15:42:16 +0900

    feat: 소비자 객체 구현

--- src/main/kotlin/christmas/domain/Customer.kt
+++ src/main/kotlin/christmas/domain/Customer.kt
@@ -1,9 +1,16 @@
 package christmas.domain
 
 class Customer(rawOrders: String) {
-    val order = rawOrders.split(',').map { menu ->
+    val orders = rawOrders.split(',').map { menu ->
         val nameAndQuantity = menu.split('-')
         Menu(nameAndQuantity[0], nameAndQuantity[1].toInt())
     }
 
+    fun getMatchedMenuBoard(): List<MenuBoard> {
+        return orders.mapNotNull { menu ->
+            MenuBoard.entries.find { menuBoard ->
+                menu.menuName == menuBoard.menuName
+            }
+        }
+    }
 }
