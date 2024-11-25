46207b1f216beba44e8883fcaa07a639ba0aaa8c
박지민 <pjm5489@naver.com>
2024-11-25 20:42:12 +0900

    feat: 예외처리 로직 추가

--- src/main/kotlin/christmas/domain/Customer.kt
+++ src/main/kotlin/christmas/domain/Customer.kt
@@ -8,6 +8,7 @@ class Customer(rawOrders: String) {
 
     private fun validateQuantity(rawQuantity: String): Int {
         val quantity = rawQuantity.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+        if (quantity > 20) throw IllegalArgumentException("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.")
         return quantity
     }
 
@@ -17,7 +18,15 @@ class Customer(rawOrders: String) {
         return menuName
     }
 
+    private fun validateDuplicateNames(rawOrders: String) {
+        val menuNames = rawOrders.split(',').map { menu ->
+            menu.split('-')[0]
+        }
+        if (menuNames.toSet().size != menuNames.size) throw IllegalArgumentException("[ERROR] 중복된 주문입니다. 다시 입력해 주세요.")
+    }
+
     private fun validateOrders(rawOrders: String): List<MenuDto> {
+        validateDuplicateNames(rawOrders)
         return rawOrders.split(',').map { menu ->
             val nameAndQuantity = menu.split('-')
             MenuDto(validateMenuName(nameAndQuantity[0]), validateQuantity(nameAndQuantity[1]))
