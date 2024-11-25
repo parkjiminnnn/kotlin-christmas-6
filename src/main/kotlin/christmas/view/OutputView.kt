3d6ec7c111f5df51b73687ef553f711044efbb86
박지민 <pjm5489@naver.com>
2024-11-25 18:12:33 +0900

    feat: 출력화면 구현

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -1,4 +1,86 @@
 package christmas.view
 
+import christmas.dto.MenuDto
+
 class OutputView {
+    private fun discountPreviewMessage(reserveDate: Int) {
+        println("12월 ${reserveDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n")
+    }
+
+    private fun printOrders(orders: List<MenuDto>) {
+        println("<주문 메뉴>")
+        orders.forEach { menuDto ->
+            println(menuDto.toString())
+        }
+        println()
+    }
+
+    private fun printBeforeDiscountPrice(beforeDiscountPrice: Int) {
+        println("<할인 전 총주문 금액>")
+        println("${beforeDiscountPrice}원\n")
+    }
+
+    private fun printFreeMenu(beforeDiscountPrice: Int) {
+        println("<증정 메뉴>")
+        if (beforeDiscountPrice >= 120000) return println("샴페인 1개\n")
+        return println("없음\n")
+
+    }
+
+    private fun printDiscount(
+        christmasDiscount: Int,
+        weekdayDiscount: Int,
+        weekendDiscount: Int,
+        specialDiscount: Int,
+        freeEventDiscount: Int
+    ) {
+        println("<혜택 내역>")
+        if (christmasDiscount != 0) println("크리스마스 디데이 할인: ${christmasDiscount}원")
+        if (weekdayDiscount != 0) println("평일 할인: ${weekdayDiscount}원")
+        if (weekendDiscount != 0) println("주말 할인:${weekendDiscount}원")
+        if (specialDiscount != 0) println("특별 할인: ${specialDiscount}원")
+        if (freeEventDiscount == 0) println("없음") else println("증정 이벤트: -25000원")
+        if (christmasDiscount != 0 && weekdayDiscount != 0 && weekendDiscount != 0 &&
+            specialDiscount != 0 && freeEventDiscount == 0
+        ) println("0원")
+            println()
+    }
+
+    private fun printTotalDiscount(totalDiscount: Int) {
+        println("<총혜택 금액>")
+        println("${totalDiscount}원\n")
+    }
+
+    private fun printAfterDiscountPrice(afterDiscountPrice: Int) {
+        println("<할인 후 예상 결제 금액>")
+        println("${afterDiscountPrice}원\n")
+    }
+
+    private fun printEventBatch(batch: String) {
+        println("<12월 이벤트 배지>")
+        println(batch)
+    }
+
+    fun printTotalResult(
+        reserveDate: Int,
+        orders: List<MenuDto>,
+        beforeDiscountPrice: Int,
+        christmasDiscount: Int,
+        weekdayDiscount: Int,
+        weekendDiscount: Int,
+        specialDiscount: Int,
+        freeEventDiscount: Int,
+        totalDiscount: Int,
+        afterDiscountPrice: Int,
+        batch: String
+    ) {
+        discountPreviewMessage(reserveDate)
+        printOrders(orders)
+        printBeforeDiscountPrice(beforeDiscountPrice)
+        printFreeMenu(beforeDiscountPrice)
+        printDiscount(christmasDiscount, weekdayDiscount, weekendDiscount, specialDiscount, freeEventDiscount)
+        printTotalDiscount(totalDiscount)
+        printAfterDiscountPrice(afterDiscountPrice)
+        printEventBatch(batch)
+    }
 }
